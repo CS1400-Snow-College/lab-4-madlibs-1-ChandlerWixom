e96886a6703b8c805f806e2a1960976a091bd7d8
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-02-19 13:36:47 -0700

    Greating

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Chandler Wixom, 2/19/2025, Lab 4 Madlibs
+
+Console.WriteLine("This is the game Mad Libs, you will be requested to provide differnt types of words\nThese words will then be combined in a pre-existing sentance to make a funny story\nEnter the requested word types");
+
--- obj/Debug/net9.0/lab-4-madlibs-1-ChandlerWixom.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-4-madlibs-1-ChandlerWixom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-4-madlibs-1-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a3b15051ebefce6ac9f7d8400df0512eed91ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1940eb71fcddc71070ebe12cd7ce8f70a1374bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-4-madlibs-1-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-4-madlibs-1-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
